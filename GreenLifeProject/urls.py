1f691b81cd4cfe568d950372551ce946792f2799
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-02-19 21:22:17 +0500

    change

--- GreenLifeProject/urls.py
+++ GreenLifeProject/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('greenlife/security-system/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('greenlife/main/', include("main.urls")),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
