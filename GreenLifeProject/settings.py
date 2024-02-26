f545a3163aab2e91959e539191d6078be2e06b83
khayrullodev <khayrullodev@khayrullodev.khayrullodev>
2024-02-26 19:03:01 +0500

    same change

--- GreenLifeProject/settings.py
+++ GreenLifeProject/settings.py
@@ -161,8 +161,8 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
